62dfbbe18e86ee1d24320ae1d7ba3549a78d0ed5
Nicolás Guzmán <n3kguzman@gmail.com>
2024-06-13 18:29:19 -0400

    agregando carrito y login al navbar

--- src/components/Home/Mostrar.jsx
+++ src/components/Home/Mostrar.jsx
@@ -22,7 +22,7 @@ const Mostrar = () => {
                       
                         <p className='ms-auto me-auto mt-4'>We have the most complete stock of guitars for you!</p>
                         <p className='ms-3 me-3 mt-4'>Discover the epitome of musical craftsmanship with our exclusive range of guitars. Whether you're a seasoned performer or an aspiring artist, our guitars are designed to elevate your sound and inspire your creativity.</p>
-                        <p className='ms-3 me-3 mt-4'>Discover the music of your soul with one of our guitars, and feel free to create expand your GAS</p>
+                        <p className='ms-3 me-3 mt-4'>Discover the music of your soul with one of our guitars, and feel free to expand your GAS</p>
                         <h2 className='ms-auto me-auto mt-5'> Guitar Shop  <FaGuitar /></h2>
                     </div>
                 </div></div>
--- src/components/Home/Navbar.jsx
+++ src/components/Home/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { FaGuitar } from "react-icons/fa6";
-
+import { IoMdCart } from "react-icons/io";
 
 
 const Navbar = () => {
@@ -12,33 +12,47 @@ const Navbar = () => {
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
+                    <form className="d-flex " role="search">
+                            <input className="form-control me-2 " type="search" placeholder="Search" aria-label="Search" />
+                            <button className="btn btn-dark " type="submit">Search</button>
+                        </form>
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav mx-auto mb-2 mb-lg-0">
                             <li className="nav-item">
                                 <a className="nav-link active" aria-current="page" href="#">Home</a>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" href="#">Link</a>
-                            </li>
-                            <li className="nav-item dropdown">
-                                <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                    Dropdown
-                                </a>
-                                <ul className="dropdown-menu">
-                                    <li><a className="dropdown-item" href="#">Action</a></li>
-                                    <li><a className="dropdown-item" href="#">Another action</a></li>
-                                    <li><hr className="dropdown-divider" /></li>
-                                    <li><a className="dropdown-item" href="#">Something else here</a></li>
-                                </ul>
+                                <a className="nav-link" href="#">Guitars</a>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link disabled" aria-disabled="true">Disabled</a>
+                                <a className="nav-link" href="#">About</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">F.A.Q</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">Contact</a>
+                            </li>
+                            <li className="nav-item dropdown">
+                            <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            <IoMdCart color='white' size={25}/>
+                            </a>
+                            <ul className="dropdown-menu">
+                                <h6 className='ms-3 mt-1'>Empty...</h6>
+                            </ul>
                             </li>
                         </ul>
-                        <form className="d-flex " role="search">
-                            <input className="form-control me-2 " type="search" placeholder="Search" aria-label="Search" />
-                            <button className="btn btn-outline-success " type="submit">Search</button>
-                        </form>
+                        
+                        <ul className="navbar-nav mx-auto mb-2 mb-lg-0">
+                            <li className="nav-item">
+                                <a className="nav-link btn btn-outline-secondary " aria-current="page" href="#">Log In</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link btn btn-outline-secondary ms-2" href="#">Sign Up</a>
+                            </li>
+                       
+                            
+                        </ul>
                     </div>
                 </div>
             </nav>
