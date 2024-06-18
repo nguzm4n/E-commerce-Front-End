15e9700338d099d789d2ea4162065f2bf35e5c07
Nicolás Guzmán <n3kguzman@gmail.com>
2024-06-18 14:18:17 -0400

    creando implementación cloudinary

--- src/App.jsx
+++ src/App.jsx
@@ -9,17 +9,34 @@ import Signup from './components/Pages/Signup'
 import Notfound from './components/Pages/Notfound'
 import Store from './components/Pages/Store'
 
+import React from 'react'
+import { Cloudinary } from '@cloudinary/url-gen';
+import { auto } from '@cloudinary/url-gen/actions/resize';
+import { autoGravity } from '@cloudinary/url-gen/qualifiers/gravity';
+import { AdvancedImage } from '@cloudinary/react';
 
 function App() {
-  const [count, setCount] = useState(0)
+
+
+  const cld = new Cloudinary({ cloud: { cloudName: 'dfhyziduf' } });
+
+  // Use this sample image or upload your own via the Media Explorer
+  const img = cld
+    .image('cld-sample-5')
+    .format('auto') // Optimize delivery by resizing and applying auto-format and auto-quality
+    .quality('auto')
+    .resize(auto().gravity(autoGravity()).width(500).height(500)); // Transform the image: auto-crop to square aspect_ratio
+
 
   return (
+
     <BrowserRouter>
-    <Navbar/>
+      <Navbar />
       <Routes>
-        <Route path="/" element={<Home  />} />
-        <Route path="/signin" element={<Login  />} />
-        <Route path="/signup" element={<Signup  />} />
+        
+        <Route path="/" element={<Home />} />
+        <Route path="/signin" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
         <Route path="/store" element={<Store />} />
         <Route path="*" element={<Notfound />} />
       </Routes>
@@ -28,3 +45,5 @@ function App() {
 }
 
 export default injectContext(App)
+
+
