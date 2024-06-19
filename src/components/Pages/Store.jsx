f888244f395324d1dc5d6eb95c29daa52ab26da5
Nicolás Guzmán <n3kguzman@gmail.com>
2024-06-19 17:50:52 -0400

    save store agregando background

--- src/components/Pages/Store.jsx
+++ src/components/Pages/Store.jsx
@@ -4,12 +4,22 @@ import { GiGuitarHead } from "react-icons/gi";
 
 const Store = () => {
 
-  
+
   return (
     <div>
       <div className='container-fluid d-flex justify-content-center bg-black'>
         <div className="row ">
           <div className="col-md-12 pt-5 pb-5 text-light titulo">   <GiGuitarHead /> |  Guitars | <GiGuitarHead /> </div>
+        </div>
+      </div>
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-md-1 bg-dark">HOLA
+          </div>
+          <div className="col-md-10 bg-secondary parrilla">HOLA
+          </div>
+          <div className="col-md-1 bg-dark">HOLA
+          </div>
         </div>
       </div>
     </div>
