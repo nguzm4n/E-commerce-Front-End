4f5f9dd2b327af1c5ebda56acdf1466b078d2aa5
Nicolás Guzmán <n3kguzman@gmail.com>
2024-06-25 11:59:09 -0400

    footer done

--- src/components/Home/Footer.jsx
+++ src/components/Home/Footer.jsx
@@ -2,30 +2,34 @@ import React from 'react'
 import { FaInstagram } from "react-icons/fa";
 import { BiLogoGmail } from "react-icons/bi";
 import { FaSquareFacebook } from "react-icons/fa6";
+import '../Styles/Home/Footer.css'
+
 const Footer = () => {
   return (
-    <div>
+    <div className='footer'>
       <div className="container-fluid">
         <div className="row">
-          <div className="col-md-4 pt-5">
-            <p>Sucursales:<br/>
+          <div className="col-md-3"></div>
+          <div className="col-md-2 pt-5 ">
+            <p className='footer-text'><b>Sucursales:</b><br/>
 
               Planet Coding Street 5948</p>
 
-              <p>Monday to Friday from 10:00 to 19:00 hrs. Saturday from 10:00 to 13:30 hrs.</p>
+              <p className='footer-text'>Monday to Friday from 10:00 to 19:00 hrs. Saturday from 10:00 to 13:30 hrs.</p>
           </div>
-          <div className="col-md-4 pt-5">
-            <p>Open since 2024<br/>
+          <div className="col-md-2 pt-5">
+            <p className='footer-text'>Open since 2024<br/>
             Monday to Friday <br/>10:00 to 19:00 <br/>
             Saturday<br/>
             10:00 to 13:00</p>
-           <p>Copyright Mukan - Mukan Industries</p>
+           <p className='footer-text-1'>Copyright Mukan - Mukan Industries</p>
           </div>
-          <div className="col-md-4 pt-5">
-            <h4>Contact</h4>
-            <p>mail@email.com</p>
-            <p>  <BiLogoGmail size={25} /> <FaInstagram  size={25}/> <FaSquareFacebook  size={25}/></p>
+          <div className="col-md-2 pt-5">
+            <h4 className='footer-h4'>Contact</h4>
+            <p className='footer-text'>mail@email.com</p>
+            <p className='footer-text'>  <BiLogoGmail size={25} /> <FaInstagram  size={25}/> <FaSquareFacebook  size={25}/></p>
           </div>
+          <div className="col-md-3"></div>
         </div>
       </div>
     </div>
